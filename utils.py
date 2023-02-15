3025e2199e07d1acec2806356868dd5b06618ef9
oumaimabenyaich <21237@ecam.be>
2023-02-15 11:10:17 +0100

    Last changes utils

--- utils.py
+++ utils.py
@@ -1,37 +1,26 @@
+from math import sqrt
+from scipy import integrate as itg
+
+#from scipy.integrate import quad
+
+
 def fact(n):
-	"""Computes the factorial of a natural number.
-	
-	Pre: -
-	Post: Returns the factorial of 'n'.
-	Throws: ValueError if n < 0
-	"""
-	pass
+	if n < 2:
+		return 1
+	else:
+		return n*fact(n-1)
+
 
 def roots(a, b, c):
-	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
-	
-	Pre: -
-	Post: Returns a tuple with zero, one or two elements corresponding
-		to the roots of the ax^2 + bx + c polynomial.
-	"""
-	pass
+	d=b*b-4*a*c
+	if d>0:
+ 		return [(-b-sqrt(d))/(2*a),(-b+sqrt(d))/(2*a)]
+	elif d == 0 : 
+		return -b/2*a
 
-def integrate(function, lower, upper):
-	"""Approximates the integral of a fonction between two bounds
-	
-	Pre: 'function' is a valid Python expression with x as a variable,
-		'lower' <= 'upper',
-		'function' continuous and integrable between 'lower‘ and 'upper'.
-	Post: Returns an approximation of the integral from 'lower' to 'upper'
-		of the specified 'function'.
+#def integrate(function, lower, upper):
 
-	Hint: You can use the 'integrate' function of the module 'scipy' and
-		you'll probably need the 'eval' function to evaluate the function
-		to integrate given as a string.
-	"""
-	pass
-
-if __name__ == '__main__':
+if __name__=='__main__' : 
 	print(fact(5))
 	print(roots(1, 0, 1))
-	print(integrate('x ** 2 - 1', -1, 1))
+	# print(integrate('x ** 2 - 1', -1, 1))
